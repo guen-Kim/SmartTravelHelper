479c47b02d222d78885c4d6da34d6d2522b5bd2c
guen <guen5997@naver.com>
2023-03-30 11:21:35 +0900

    초기 환경 설정

---
+++ app/src/main/java/org/techtown/smart_travel_helper/DrowsinessActicity.kt
@@ -0,0 +1,28 @@
+package org.techtown.smart_travel_helper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DrowsinessActicity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drowsiness_detection)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

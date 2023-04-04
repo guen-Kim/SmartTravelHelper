7346d27c23b4fb29d2678fe9144d5f2899eb3892
guen <guen5997@naver.com>
2023-04-04 19:59:31 +0900

    TmapAPI 사용환경 설정, color 선언 및 themes 정의

--- app/src/main/java/org/techtown/smart_travel_helper/ui/MainActivity.kt
+++ app/src/main/java/org/techtown/smart_travel_helper/ui/MainActivity.kt
@@ -2,13 +2,28 @@ package org.techtown.smart_travel_helper.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import org.techtown.smart_travel_helper.R
+import org.techtown.smart_travel_helper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var binidng:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // binding data
+        binidng = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+        //인증 요청
+        binidng.tmap.setSKTMapApiKey("j1MKrD2Bcx2Vt1ibRJsO1akBYm4Rr9uV1UvSSja5")
 
+
+        // TODO: 사용자 GPS 정보 받기
     }
 }
